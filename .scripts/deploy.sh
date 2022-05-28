6d38dbed8f8911cb5efae5bd1435a93931311962
Ozair Patel <ozairpatel2@gmail.com>
2022-05-27 20:25:50 -0500

    Add GH actions, lighten background video in welcome.blade.php

---
+++ .scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Change ownership of vendor so www-data can access them
+chmod -R www-data vendor
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Run database migrations
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
             left: 0;
             height: 100%;
             width: 100%;
-            background-color: rgba(0, 0, 0, 0.90);
+            background-color: rgba(0, 0, 0, 0.85);
         }
 
         .background-video video {
